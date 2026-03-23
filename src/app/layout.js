e09dde4e2c774504892dd0b5316e40fd55bcb710
tldasarath <tldasarath@gmail.com>
2026-03-23 13:17:23 +0530

    fix:google tag issue fixed

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,6 +16,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+        <head>
+        <script
+          src="https://www.googletagmanager.com/gtag/js?id=G-8E5N4PMC1Q"
+          strategy="afterInteractive"
+        />
+        <script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-8E5N4PMC1Q');
+          `}
+        </script>
+      </head>
       <body className="font-sans">
         <Toaster position="top-right" />
         {children}
